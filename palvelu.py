78fc89f74a636baa836086c9afde41a480b9e89f
Mihkel Palta <Mihkel.Palta@edu.mayk.fi>
2025-03-14 11:51:16 +0200

    asia ja painike

--- palvelu.py
+++ palvelu.py
@@ -2,14 +2,12 @@ from wsgiref.simple_server import make_server
 import time as t
 
 def app(environ, respond):
-    respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    for i in range(100):
-        t.sleep(1)
-        if i%2 == 0:
-            yield "Hello w€rld😞!".encode('utf-8')
-        else:
-            yield "Badbye w€rld😞!".encode('utf-8')
-            
+    respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
+    nimi = environ['PATH_INFO'].strip('/')
+    salanimi = nimi.replace('a', 'apa').replace('i', 'ipi').replace('n', 'non').replace('na', 'nana')
+    yield ("salainen nimesi on: %s\n" % salanimi).encode('utf-8')
+    yield "<form method=GET><input type=button value=paina></form>".encode("utf-8")
+
 if __name__ == '__main__':
-    with make_server("172.18.1.1", 8000, app) as server: 
+    with make_server("localhost", 8000, app) as server: 
         server.serve_forever()
